bd0e2c1de57042bbb4b11481d8b672d355315eb6
Iryna220 <ibiriszka951@gmail.com>
2023-08-05 20:07:58 +0200

    home work -1
    new repoz.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { Profile } from './Profile/Profile';
+import user from 'data-components/user.json';
+
+import { Statistics } from './Statistics/Statistics';
+import data from 'data-components/data.json';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from 'data-components/friends.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from 'data-components/transactions.json';
+
+
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+      </>
   );
 };
